cc4aa868e90714259594befc5fd8e65e3319972d
priti <pritipralipta18@gmail.com>
2023-12-22 00:25:35 +0530

    greatest among 3 numbers

---
+++ greatest.py
@@ -0,0 +1,11 @@
+a=int(input("a="))
+b=int(input("b=")) 
+c=int(input("c="))
+if a>=b and a>=c:
+    print("a is greatest")
+elif b>=a and b>=c:
+    print("b is greatest")
+elif c>=b and c>=a:
+    print("c is greatest")
+else:
+    print("all are equal")
